5119ba4c6de40f462884bac918a3cf716b0fcfb0
Luiz Felipe <127757663+Luiz07102007@users.noreply.github.com>
2025-03-19 20:42:14 -0300

    Adicionando os arquivos no repositório
    adicionei os arquivos pela primeira vez ao repositório

---
+++ App.js
@@ -0,0 +1,113 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Image,TouchableOpacity } from 'react-native';
+
+export default function App() {
+  const [num1, setNum1] = useState('');
+  const [num2, setNum2] = useState('');
+  const [resultado, setResultado] = useState('');
+  const calc2 = (op) => {
+    const n3 = parseFloat(num3);
+    
+  }
+  const calcular = (vm) => {
+    const n1 = parseFloat(num1);
+    const n2 = parseFloat(num2);
+
+    if (isNaN(n1) || isNaN(n2)) {
+      setResultado('Entrada inválida');
+      return;
+    }
+
+    switch (vm) {
+      case 'conta':
+        setResultado(n1/n2);
+        break;
+      default:
+        break;
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Image
+        source={{ uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTz-UBos782-oUg_4Ps3MqFvo5yqHwslXFwCA&s' }}
+        style={styles.image}
+      />
+      <Text style={styles.title}>Velocidade Média Fórmula</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Digite o valor da distância percorrida"
+        keyboardType="numeric"
+        value={num1}
+        onChangeText={(text) => setNum1(text)}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Digite o valor do tempo"
+        keyboardType="numeric"
+        value={num2}  
+        onChangeText={(text) => setNum2(text)}
+      />
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.button} onPress={() => calcular('conta')}>
+          <Text style={styles.buttonText}>Calcular</Text>
+        </TouchableOpacity>
+      </View>
+
+      <Text style={styles.result}>O valor da velocidade média é: {resultado} V</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f5f5f5',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+  },
+  image: {
+    width: 186,
+    height: 271,
+    marginBottom: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#333',
+    marginBottom: 20,
+  },
+  input: {
+    width: '80%',
+    borderWidth: 1,
+    borderColor: '#ccc',
+    padding: 10,
+    marginVertical: 10,
+    borderRadius: 5,
+    backgroundColor: '#fff',
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginVertical: 20,
+    width: '80%',
+  },
+  button: {
+    backgroundColor: '#4CAF50',
+    padding: 10,
+    borderRadius: 5,
+    marginHorizontal: 5,
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  result: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#333',
+    marginBottom: 20,
+  },
+});
